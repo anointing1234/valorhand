975103a3fb91429860392dcd8ee229137e2402cf
anointing adebayo <124325944+anointing1234@users.noreply.github.com>
2024-10-14 22:25:01 +0100

    fix allowed host

--- valorhand/settings.py
+++ valorhand/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# Security settings
 ALLOWED_HOSTS = ["web-production-23342.up.railway.app"]
+
+CSRF_TRUSTED_ORIGINS = ["https://web-production-23342.up.railway.app"]
+
 
 AUTH_USER_MODEL = 'accounts.Account'
 
