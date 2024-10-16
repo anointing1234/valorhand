abd07aa6ed6ff471d7bd4383cc56f8314e578a80
anointing adebayo <124325944+anointing1234@users.noreply.github.com>
2024-10-16 13:48:19 +0100

    fixed issue

--- valorhand/settings.py
+++ valorhand/settings.py
@@ -33,12 +33,12 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = False
 
 # Security settings
-# ALLOWED_HOSTS = ["web-production-23342.up.railway.app",'127.0.0.1']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["web-production-23342.up.railway.app"]
 
 
 
-# CSRF_TRUSTED_ORIGINS = ["https://web-production-23342.up.railway.app"]
+
+CSRF_TRUSTED_ORIGINS = ["https://web-production-23342.up.railway.app"]
 
 
 AUTH_USER_MODEL = 'accounts.Account'
